23a1e8aa34e1a57b777731ed5e806179e5684b4c
Marioluigiman <mikipux7@gmail.com>
2018-10-28 17:56:57 -0500

    First Commit for Rube Goldberg Project

---
+++ Assets/pully.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pully : MonoBehaviour {
+
+    private Rigidbody rb;
+
+    // Use this for initialization
+    void Start() {
+        rb = GetComponent<Rigidbody>();
+        rb.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ | RigidbodyConstraints.FreezePosition;
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+       }
+}
